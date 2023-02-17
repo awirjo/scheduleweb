3000b027e130aa554bb215cb7404e904734944b7
awirjo <a.wirjomenggolo@unasat.sr>
2023-02-16 22:47:27 -0300

    Update 2.1

--- src/main/webapp/breakTime.js
+++ src/main/webapp/breakTime.js
@@ -1,22 +1,55 @@
-function addBreakTime() {
-    document.getElementById("breaktime-form").addEventListener("submit", function (event) {
-        event.preventDefault();
-        var xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function () {
-            if (this.readyState == 4 && this.status == 200) {
-                alert("BreakTime added successfully!");
-            }
-        };
-        xhttp.open("POST", "http://localhost:8081/scheduleweb_war_exploded/api/breakTime/addBreak", true);
-        xhttp.setRequestHeader("Content-Type", "application/json");
-        var breakTime = {
-            "serving_time": document.getElementById("serving_time").value,
-            "serving_date": document.getElementById("serving_date").value,
-            "menu": {"id": document.getElementById("menu").value},
-            "department": [{"id": document.getElementById("department").value}]
-        };
-        xhttp.send(JSON.stringify(breakTime));
+function saveBreakTime() {
+    var menu = document.getElementById("menu").value;
+    var department = document.getElementById("department-ids").value;
+    var servingDate = document.getElementById("serving_date").value;
+    var servingTime = document.getElementById("serving_time").value;
+
+    var data = {
+        serving_date: servingDate,
+        serving_time: servingTime,
+        menu: { id: menu },
+        department: [{ id: department }],
+    };
+
+    $.ajax({
+        type: "POST",
+        contentType: "application/json",
+        url: "http://localhost:8081/scheduleweb_war_exploded/api/breakTime/addBreak",
+        data: JSON.stringify(data),
+        dataType: "json",
+        success: function (data) {
+            alert("Breaktime added successfully");
+            $("#addBreakTimeModal").modal("hide");
+            clearForm();
+        },
+        error: function (e) {
+            alert("Error adding breaktime. Please try again.");
+            console.log("ERROR : ", e);
+        },
     });
-
-
 }
+
+function submitBreaktimeForm() {
+    var form = document.getElementById("breaktime-form");
+    var formData = new FormData(form);
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "http://localhost:8081/scheduleweb_war_exploded/api/breakTime/addBreak");
+
+    xhr.onload = function () {
+        if (xhr.status == 200) {
+            alert("Breaktime added successfully!");
+            form.reset();
+        } else {
+            alert("Error adding breaktime");
+        }
+    };
+
+    xhr.send(formData);
+}
+
+var form = document.getElementById("breaktime-form");
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    submitBreaktimeForm();
+});
